6626a5f2c10be83e7e3299c8caf0bcc04dc79a8d
iTzFabregas <fabricio-sampaio@usp.br>
2024-12-17 19:02:21 -0300

    perf: made a for loop to display the divs

--- calc-vocacional/src/components/perguntas/P1.jsx
+++ calc-vocacional/src/components/perguntas/P1.jsx
@@ -5,6 +5,13 @@ import img1 from '/perg0_img1.jpeg'
 import img2 from '/perg0_img2.jpeg'
 import img3 from '/perg0_img3.jpeg'
 
+const images = [
+    { image: img0, label: 'Grandes construções' },
+    { image: img1, label: 'Programação e computação' },
+    { image: img2, label: 'Problemas teóricos' },
+    { image: img3, label: 'Desenvolvimento sustentável' }
+]
+
 function P1({ setfaseAtual, faseAtual }) {
 
     // cada index se refere a uma imagem
@@ -50,72 +57,34 @@ function P1({ setfaseAtual, faseAtual }) {
     }
 
     return (
-        <div className='h-full w-full'>
-            <h2>Parte I - Preferência Visual </h2>
-            <h3>Selecione os campos de atuação que mais  te interessar e menos te interessar</h3>
+        <div className='h-full w-full flex flex-col justify-between items-center'>
+            <div>
+                <h2>Parte I - Preferência Visual </h2>
+                <h3>Selecione os campos de atuação que mais  te interessar e menos te interessar</h3>
+            </div>
             <div className='flex justify-center text-center items-center'>
                 <div className="grid grid-cols-1 gap-y-8 h-full place-items-center md:grid-cols-2">
-                    <div className={`relative group w-1/2 border-2 ${getBorderColor(0)}`} 
-                        onMouseEnter={() => changeHover(0)} onMouseLeave={() => changeHover(0)}>
 
-                        <img src={img0} alt="Grandes construções"
-                            className={`w-full h-full object-cover transition duration-300 ${hover[0] ? "blur-md" : "blur-0"}`}/>
-                        {hover[0] && (
-                            <div className="absolute inset-0 flex items-center justify-center space-x-4">
-                                <button onClick={() => changeInteresse(0, '1')} className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded shadow"> Interesso </button>
+                    {images.map((img, index) => (
+                        <div key={index} className={`relative group w-1/2 border-2 ${getBorderColor(index)}`} onMouseEnter={() => changeHover(index)} onMouseLeave={() => changeHover(index)}>
 
-                                <button onClick={() => changeInteresse(0, '0')} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow"> Não interesso </button>
-                            </div>
-                        )}
+                            <img src={img['image']} alt={img['label']} className={`w-full h-full object-cover transition duration-200 ${hover[index] ? "blur-md" : "blur-0"}`} />
+                            {hover[index] && (
+                                <div className="absolute inset-0 flex items-center justify-center space-x-4">
+                                    <button onClick={() => changeInteresse(index, '1')} className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded shadow"> Interesso </button>
 
-                    </div>
-                    <div className={`relative group w-1/2 border-2 ${getBorderColor(1)}`} 
-                        onMouseEnter={() => changeHover(1)} onMouseLeave={() => changeHover(1)}>
+                                    <button onClick={() => changeInteresse(index, '0')} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow"> Não interesso </button>
+                                </div>
+                            )}
 
-                        <img src={img1} alt="Programação e computação"
-                            className={`w-full h-full object-cover transition duration-300 ${hover[1] ? "blur-md" : "blur-0"}`}/>
-                        {hover[1] && (
-                            <div className="absolute inset-0 flex items-center justify-center space-x-4">
-                                <button onClick={() => changeInteresse(1, '1')} className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded shadow"> Interesso </button>
+                        </div>
+                    ))}
 
-                                <button onClick={() => changeInteresse(1, '0')} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow"> Não interesso </button>
-                            </div>
-                        )}
-                        
-                    </div>
-                    <div className={`relative group w-1/2 border-2 ${getBorderColor(2)}`} 
-                        onMouseEnter={() => changeHover(2)} onMouseLeave={() => changeHover(2)}>
-
-                        <img src={img2} alt="Problemas teóricos"
-                            className={`w-full h-full object-cover transition duration-300 ${hover[2] ? "blur-md" : "blur-0"}`}/>
-                        {hover[2] && (
-                            <div className="absolute inset-0 flex items-center justify-center space-x-4">
-                                <button onClick={() => changeInteresse(2, '1')} className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded shadow"> Interesso </button>
-
-                                <button onClick={() => changeInteresse(2, '0')} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow"> Não interesso </button>
-                            </div>
-                        )}
-                        
-                    </div>
-                    <div className={`relative group w-1/2 border-2 ${getBorderColor(3)}`} 
-                        onMouseEnter={() => changeHover(3)} onMouseLeave={() => changeHover(3)}>
-
-                        <img src={img3} alt="Desenvolvimento sustentável"
-                            className={`w-full h-full object-cover transition duration-300 ${hover[3] ? "blur-md" : "blur-0"}`}/>
-                        {hover[3] && (
-                            <div className="absolute inset-0 flex items-center justify-center space-x-4">
-                                <button onClick={() => changeInteresse(3, '1')} className="bg-green-500 hover:bg-green-600 text-white px-4 py-2 rounded shadow"> Interesso </button>
-
-                                <button onClick={() => changeInteresse(3, '0')} className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded shadow"> Não interesso </button>
-                            </div>
-                        )}
-                        
-                    </div>
                 </div>
             </div>
 
             <button onClick={proxPergunta}
-                className='p-1 bg-gray-300 rounded-lg m-5'>
+                className='p-1 px-5 bg-gray-300 rounded-lg m-5 w-fit'>
                 Próxima Pergunta
             </button>
         </div>
