bbaceaa4b75508adb47ede2be5940efc01af4cf3
iTzFabregas <fabricio-sampaio@usp.br>
2024-12-17 19:02:53 -0300

    feat: select question template done

--- calc-vocacional/src/components/perguntas/P2.jsx
+++ calc-vocacional/src/components/perguntas/P2.jsx
@@ -1,12 +1,54 @@
+import { useState } from 'react'
+
+const options = [
+    "Me interesso por entender a interação de elementos a um nível atômico.",
+    "Sinto facilidade em assistir a aulas e ensinar meus colegas.",
+    "Entendo facilmente assuntos como álgebra e geometria.",
+    "Me interesso pelas áreas de botânica e ecologia.",
+    "Busco aprender sobre programação.",
+    "Gosto de entender a interpretação física de fenômenos.",
+    "Tenho curiosidade sobre como máquinas e dispositivos funcionam.",
+    "Me interesso em usar a tecnologia para construir algo novo, como aplicativos ou dispositivos.",
+];
+
 function P2({ setfaseAtual, faseAtual }) {
+
+    const [checkedItems, setCheckedItems] = useState([]);
+
+
+
+    const handleCheckboxChange = (index) => {
+        setCheckedItems((prev) => {
+            if (prev.includes(index)) return prev.filter((item) => item !== index);
+            if (prev.length < 3) return [...prev, index];
+            alert("Não é possível selecionar mais de 3 frases")
+            return prev;
+        });
+    };
 
 
     return (
-        <div>
-            <h2>Pergunta 2</h2>
-            <p>22222222222222222222222222222222222222222</p>
+        <div className='h-full w-full flex flex-col justify-between text-center items-center'>
+            <div>
+                <h2>Parte 2 - Avaliação de interesses </h2>
+                <h3>Escolha as três frases com as quais você mais se identifica</h3>
+            </div>
+
+            <div className='flex flex-col gap-5 items-start w-fit text-2xl'>
+                
+                <h1 className='font-bold text-3xl'>Durante a escola, eu:</h1>
+
+                {options.map((label, index) => (
+                    <div key={index} className={`p-3 transition hover:bg-gray-100 ${checkedItems.includes(index) ? "bg-blue-100" : ""}`}>
+                        <input type="checkbox" checked={checkedItems.includes(index)} onChange={() => handleCheckboxChange(index)} className="mr-5 w-6 h-6" />
+                        <label>{label}</label>
+                    </div>
+                ))}
+
+            </div>
+
             <button onClick={() => setfaseAtual(faseAtual + 1)}
-                className='p-1 bg-gray-300 rounded-lg m-5'>
+                className='p-1 px-5 bg-gray-300 rounded-lg m-5'>
                 Próxima Pergunta
             </button>
         </div>
