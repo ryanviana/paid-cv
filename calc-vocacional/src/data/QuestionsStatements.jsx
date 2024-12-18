fb682a7dfc70965f2b39a01aad1e44891ed41c38
iTzFabregas <fabricio-sampaio@usp.br>
2024-12-18 13:11:29 -0300

    feat: image questions score

---
+++ calc-vocacional/src/data/QuestionsStatements.jsx
@@ -0,0 +1,11 @@
+import question_img0 from '/perg0-img0.jpeg'
+import question_img1 from '/perg0_img1.jpeg'
+import question_img2 from '/perg0_img2.jpeg'
+import question_img3 from '/perg0_img3.jpeg'
+
+export const statement_question_1 = [
+    { image: question_img0, label: 'Grandes construções' },
+    { image: question_img1, label: 'Programação e computação' },
+    { image: question_img2, label: 'Problemas teóricos' },
+    { image: question_img3, label: 'Desenvolvimento sustentável' }
+]
