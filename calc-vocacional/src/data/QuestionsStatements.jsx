e4288f40451c412fd410deb52d6d2589479730ab
iTzFabregas <fabricio-sampaio@usp.br>
2024-12-19 13:12:59 -0300

    file: add constants

--- calc-vocacional/src/data/QuestionsStatements.jsx
+++ calc-vocacional/src/data/QuestionsStatements.jsx
@@ -1,14 +1,52 @@
-import question_img0 from '/perg0-img0.jpeg'
-import question_img1 from '/perg0_img1.jpeg'
-import question_img2 from '/perg0_img2.jpeg'
-import question_img3 from '/perg0_img3.jpeg'
-
+import question1_img0 from '/perg1_img0.jpeg'
+import question1_img1 from '/perg1_img1.jpeg'
+import question1_img2 from '/perg1_img2.jpeg'
+import question1_img3 from '/perg1_img3.jpeg'
 export const statement_question_1 = [
-    { image: question_img0, label: 'Grandes construções' },
-    { image: question_img1, label: 'Programação e computação' },
-    { image: question_img2, label: 'Problemas teóricos' },
-    { image: question_img3, label: 'Desenvolvimento sustentável' }
+    { image: question1_img0, label: 'Grandes construções' },
+    { image: question1_img1, label: 'Programação e computação' },
+    { image: question1_img2, label: 'Problemas teóricos' },
+    { image: question1_img3, label: 'Desenvolvimento sustentável' }
 ]
+
+
+import question2_img0 from '/perg2_img0.jpeg'
+import question2_img1 from '/perg2_img1.jpeg'
+import question2_img2 from '/perg2_img2.jpeg'
+import question2_img3 from '/perg2_img3.jpeg'
+export const statement_question_2 = [
+    { image: question2_img0, label: 'Aeronaves' },
+    { image: question2_img1, label: 'Sistemas elétricos' },
+    { image: question2_img2, label: 'Automação e robótica' },
+    { image: question2_img3, label: 'Ciência dos materiais' }
+]
+
+
+import question3_img0 from '/perg3_img0.jpeg'
+import question3_img1 from '/perg3_img1.jpeg'
+import question3_img2 from '/perg3_img2.jpeg'
+import question3_img3 from '/perg3_img3.jpeg'
+export const statement_question_3 = [
+    { image: question3_img0, label: 'Laboratórios de pesquisa' },
+    { image: question3_img1, label: 'Escritórios' },
+    { image: question3_img2, label: 'Campos abertos' },
+    { image: question3_img3, label: 'Obras' }
+]
+
+
+import question4_img0 from '/perg4_img0.jpeg'
+import question4_img1 from '/perg4_img1.jpeg'
+import question4_img2 from '/perg4_img2.jpeg'
+import question4_img3 from '/perg4_img3.jpeg'
+export const statement_question_4 = [
+    { image: question4_img0, label: 'Sala de aula' },
+    { image: question4_img1, label: 'Fábrica' },
+    { image: question4_img2, label: 'Laboratório' },
+    { image: question4_img3, label: 'Simuladores de voo' }
+]
+
+
+
 
 export const statement_question_5 = [
     "Me interesso por entender a interação de elementos a um nível atômico.",
@@ -21,3 +59,62 @@ export const statement_question_5 = [
     "Me interesso em usar a tecnologia para construir algo novo, como aplicativos ou dispositivos.",
 ];
 
+
+export const statement_question_6 = [
+    "Trabalhar em projetos que ajudem a preservar o meio ambiente.",
+    "Criar soluções tecnológicas inovadoras que impactem a sociedade.",
+    "Ter uma carreira sólida em uma área de alta demanda no mercado.",
+    "Trabalhar diretamente em projetos práticos e aplicados.",
+    "Fazer pesquisas científicas e descobrir novas ideias ou tecnologias.",
+    "Criar ou gerenciar minha própria empresa ou startup.",
+    "Trabalhar em grandes projetos, como construções ou infraestrutura.",
+    "Contribuir para melhorar a vida das pessoas com meu trabalho.",
+];
+
+
+export const statement_question_7 = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+];
+
+
+export const statement_question_8 = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+];
+
+
+export const statement_question_9 = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+];
+
+
+export const statement_question_10 = [
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+];
