eeb8bdf3c7c4ac0e96bc2e608457689aa9306504
iTzFabregas <fabricio-sampaio@usp.br>
2024-12-25 20:37:44 -0300

    feat: creating the graphic in the frontend

---
+++ calc-vocacional/src/components/Grafico.jsx
@@ -0,0 +1,67 @@
+import { Radar } from 'react-chartjs-2';
+import { Chart as ChartJS, RadialLinearScale, CategoryScale, LinearScale, PointElement, LineElement, Filler, Tooltip, Legend } from 'chart.js';
+
+// Registrar as escalas e elementos necessários
+ChartJS.register(
+    RadialLinearScale,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Filler,
+    Tooltip,
+    Legend
+);
+
+const options = {
+    responsive: true,
+    scales: {
+        r: {
+            ticks: {
+                display: true,
+            },
+            angleLines: {
+                display: true,
+            },
+            pointLabels: {
+                font: {
+                    size: 15,
+                    weight: 'bold',
+                    family: 'Arial',
+                },
+            },
+        },
+    },
+    plugins: {
+        legend: {
+            display: false
+        },
+    },
+    animation: {
+        duration: 500,
+        easing: 'easeOutCubic',
+    },
+};
+
+function Grafico({ pontuacaoTotal, type }) {
+
+    let labels = ["Computacao", "Eletrica", "Mecatronica", "Aeronautica", "Licenciatura", "Producao", "Materiais", "Civil", "Ambiental"]
+    if (type === 'parcial') labels = ["???", "???", "???", "???", "???", "???", "???", "???", "???"]
+
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: "Score",
+            data: pontuacaoTotal,
+            backgroundColor: 'rgba(6, 253, 253, 0.5)',
+            borderColor: '#04caca',
+            borderWidth: 3
+        }]
+    };
+
+    return (
+        <Radar data={data} options={options} />
+    );
+}
+
+export default Grafico;
---
+++ calc-vocacional/src/pages/ResultFront.jsx
@@ -0,0 +1,21 @@
+import Grafico from "../components/Grafico";
+
+function ResultFront({ updatePerguntaAtual, pontuacaoTotal, type }) {
+
+    return (
+        <div className="w-full h-full flex flex-col justify-between items-center mb-10">
+
+            <div className="flex justify-center text-center items-center w-full h-[90%]">
+                <Grafico pontuacaoTotal={pontuacaoTotal} type={type} />
+            </div>
+
+            <button onClick={() => updatePerguntaAtual()}
+                className='px-5 bg-gray-300 rounded-lg transition-all duration-100 ease-in-out hover:bg-gray-400 hover:scale-105'>
+                Próxima Pergunta
+            </button>
+        </div>
+
+    );
+}
+
+export default ResultFront;
