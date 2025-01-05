240eaa47541225a19b77dad15fa3eba69f817d48
iTzFabregas <fabricio-sampaio@usp.br>
2025-01-05 18:01:41 -0300

    feat: add explanation for dev purposes

--- calc-vocacional/src/pages/Email.jsx
+++ calc-vocacional/src/pages/Email.jsx
@@ -4,7 +4,7 @@ import axios from 'axios'
 function Email({ pontuacaoTotal }) {
 
     const [email, setEmail] = useState('');
-    const [status, setStatus] = useState('');
+    const [status, setStatus] = useState('ready');
 
     const enviarEmail = async () => {
         try {
@@ -22,9 +22,10 @@ function Email({ pontuacaoTotal }) {
     }
 
     return (
-        <div className='flex flex-col items-center justify-center h-full gap-5'>
+        <div className='flex flex-col items-center justify-center h-full gap-2'>
             <h1 className='font-bold text-2xl'>Digite seu email</h1>
-            <input type="text" className='bg-gray-200 border-gray-400 border rounded-xl p-2' onChange={(e) => setEmail(e.target.value)}/>
+            <div className='text-sm'>Necessário liberar o site do <a href="https://3.12.246.4:4000/" className='hover:underline text-blue-700'>backend</a> (certificado auto assinado)</div>
+            <input type="text" className='bg-gray-200 border-gray-400 border rounded-xl p-2 m-3' onChange={(e) => setEmail(e.target.value)}/>
             <button className='bg-gray-300 p-2 rounded-xl w-48 transition-all hover:scale-105 hover:bg-gray-400' onClick={enviarEmail}>Enviar email</button>
  
             {status === 'loading' && <div>Enviando email...</div>}
