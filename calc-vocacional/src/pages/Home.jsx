6b713513079c4c145fe708beb4da0284317ac61b
iTzFabregas <fabricio-sampaio@usp.br>
2025-01-14 14:05:41 -0300

    refactor: routes

--- calc-vocacional/src/pages/Home.jsx
+++ calc-vocacional/src/pages/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className="flex flex-col w-full items-center">
       <h1>Home hehe</h1>
-      <Link to="/landing" className="bg-gray-300 p-3 w-fit">Ir para a Landing Page</Link>
+      <Link to="/calculoVocacional" className="bg-gray-300 p-3 w-fit">Ir para a Landing Page</Link>
     </div>
   );
 }
