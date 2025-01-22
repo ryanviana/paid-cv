f0e50fccb7ecd9d11b750be3c468c2e04bf3485e
Michel Hecker Faria-SJE016 <sje016@motorola.com>
2025-01-22 18:45:31 -0300

    fix: delete unnecessary page 'home'

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import '../styles/index.css'
 
 import Header from "../components/Header";
-import Home from "../pages/Home";
 import LandingPage from "../pages/LandingPage";
 import Questions from "../pages/Questions";
 
@@ -11,12 +10,10 @@ function App() {
 
     return (
         <div className='text-center h-screen flex flex-col justify-between'>
-            <Header />
-
             <Router>
+                <Header />
                 <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/calculoVocacional" element={<LandingPage />} />
+                    <Route path="/" element={<LandingPage />} />
                     <Route path="/questions" element={<Questions />} />
                 </Routes>
             </Router>
